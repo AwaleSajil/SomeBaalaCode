5ca59fb1a75ce243ac532597c7694d6b504cd323
SajiAwale <sajilawale@gmail.com>
2020-11-29 21:00:17 +0545

    Corrected typecast

--- RunningMotor/RunningMotor.ino
+++ RunningMotor/RunningMotor.ino
@@ -5,14 +5,12 @@ class Motor
   int motorPin; //the output pin number used for the motor
   unsigned long duration; //time for wgich motor is to be turned on (in milliseconds)
   unsigned long startTime; //time when the motor was started
-
   public:
-    Motor(int motorpin, int duration){
+    Motor(int motorpin, int runDuration){
       //constructor for the class
       //duration is sent in secounds
-      this->duration = int(duration*1000);
+      this->duration = ((unsigned long)runDuration) * 1000;
       this->motorPin = motorpin;
-
       //set the motorPin as output
       pinMode(this->motorPin, OUTPUT);
     }
@@ -36,12 +34,12 @@ class Motor
       }
     }
 
-    void Update(){
+    long Update(){
       //first check if motor is on;
       if (onState){
         //check if the time is up (given duration is finished)
         unsigned long currentTime = millis();
-        unsigned long currentDuration = currentTime - this->startTime;
+        unsigned long currentDuration = currentTime - startTime;
         if (currentDuration > (this->duration)){
           //if time is out stop the motor
           digitalWrite(this->motorPin, LOW);  // Stop the actuak motor
@@ -53,11 +51,13 @@ class Motor
 };
 
 int angleGrinderPin = 8; //physical pin for signalling motor
-int runDuration = 3 * 60;  //in seconds
+int runDuration = 3 *60;  //in seconds
+
 Motor angleGrinder(angleGrinderPin, runDuration);
 
+
 void setup() {
-//  Serial.begin(9600);
+  Serial.begin(9600);
   delay(100);
   angleGrinder.start();
 }
